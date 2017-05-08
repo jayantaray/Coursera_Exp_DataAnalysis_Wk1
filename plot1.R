2adcec322583e42be0743512ec377fa2d9ba69a7
JAYANTA RAY <jayantaray1310@gmail.com>
2017-05-08 16:14:56 +0800

    contains_codefiles_and_png_files

--- plot1.R
+++ plot1.R
@@ -1,8 +1,6 @@
 my_data <- read.table("household_power_consumption.txt", sep = ";", na.strings = "?", header = TRUE)
-hist(my_data$Global_active_power)
 power$Date <- as.Date(power$Date, format="%d/%m/%Y")
 my_data$Date <- as.Date(my_data$Date, format="%d/%m/%Y")
-power <- my_data[(my_data$Date=="2007-02-01") | (my_data$Date=="2007-02-02"),]
 power <- my_data[(my_data$Date=="2007-02-01") | (my_data$Date=="2007-02-02"),]
 power$Global_active_power <- as.numeric(as.character(power$Global_active_power))
 plot1 <- hist(power$Global_active_power, main = "Global Active Power", col="red", xlab="Global Active Power (kilowatts)")
